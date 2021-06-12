bb37dd6a3febcc87574054fed2fd86e0c63b082a
jinita123 <85737079+jinita123@users.noreply.github.com>
2021-06-12 11:30:33 +0530

    Add files via upload

---
+++ lets code data structure.py
@@ -0,0 +1,14 @@
+Python 3.9.5 (v3.9.5:0a7dcbdb13, May  3 2021, 13:05:53) 
+[Clang 12.0.5 (clang-1205.0.22.9)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> radius=input("Enter the radius of circle")
+radius=float(radius)
+area=3.14*radius*radius 
+print("The area of circle with radius",radius," is ",area)
+Enter the radius of circle 1.1
+>>> 
+>>> 
+=============== RESTART: /Users/jinitatelmasre/Documents/jini.py ===============
+Enter the radius of circle 1.1
+The area of circle with radius 1.1  is  3.7994000000000008
+>>> 
